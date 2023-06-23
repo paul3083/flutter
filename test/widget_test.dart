94e6f327dd3c83582a6648f397fba4fd11c95084
paul3083 <paul30833083@gmail.com>
2023-06-23 09:03:32 +0900

    first_calculator

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:todolist/main.dart';
+// ignore: depend_on_referenced_packages
+import 'package:project/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
