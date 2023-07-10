37914277bd2a36b386ac915bc551af7d2ef32509
paul3083 <paul30833083@gmail.com>
2023-07-10 14:21:44 +0900

    todolist_bottomnavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'calendar.dart'; // Import the calendar.dart file
+import 'package:table_calendar/table_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const TodoApp(),
-      routes: {
-        '/calendar': (context) => const Calendar(), // Add a route for the calendar screen
-      },
     );
   }
 }
@@ -50,7 +46,8 @@ class _TodoAppState extends State<TodoApp> {
     setState(() {
       _currentIndex = index;
       if (_currentIndex == 1) {
-        Navigator.pushNamed(context, '/calendar');// Navigate to the calendar screen
+        Navigator.pushNamed(
+            context, '/calendar'); // Navigate to the calendar screen
         _currentIndex = 0;
       }
     });
@@ -58,94 +55,140 @@ class _TodoAppState extends State<TodoApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Todo List"),
-      ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (_, index) {
-          final todo = todos[index];
-          return ListTile(
-            title: Text(todo.title),
-            subtitle: Text(todo.description),
-
-            trailing: IconButton(
-              icon: const Icon(Icons.check),
-              onPressed: () {
-                setState(() {
-                  todos.removeAt(index);
-                });
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text("삭제되었습니다.")),
-                );
-              },
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showDialog(
-            context: context,
-            builder: (_) {
-              return AlertDialog(
-                title: const Text('나의 할일'),
-                actions: [
-                  TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        title = value;
-                      });
-                    },
-                    decoration: const InputDecoration(hintText: "할일"),
-                  ),
-                  TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        description = value;
-                      });
-                    },
-                    decoration: const InputDecoration(hintText: "기간"),
-                  ),
-                  Center(
-                    child: ElevatedButton(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Todo List"),
+          ),
+          body: TabBarView(
+            children: [
+              ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (_, index) {
+                  final todo = todos[index];
+                  return ListTile(
+                    title: Text(todo.title),
+                    subtitle: Text(todo.description),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.check),
                       onPressed: () {
-                        Navigator.of(context).pop();
                         setState(() {
-                          todos.add(Todo(
-                            title: title,
-                            description: description,
-                          ));
+                          todos.removeAt(index);
                         });
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("삭제되었습니다.")),
+                        );
                       },
-                      child: const Text(" 추가하기"),
                     ),
+                  );
+                },
+              ),
+              TableCalendar(
+                firstDay: DateTime.utc(2021, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: DateTime.now(),
+                calendarBuilders: CalendarBuilders(
+                    markerBuilder: (context, date, dynamic event) {
+                  if (event.isNotEmpty) {
+                    return Container(
+                      width: 35,
+                      decoration: BoxDecoration(
+                          color: Colors.blueAccent.withOpacity(0.2),
+                          shape: BoxShape.circle),
+                    );
+                  }
+                  return null;
+                }),
+              )
+            ],
+          ),
+          extendBodyBehindAppBar: true,
+          bottomNavigationBar: Container(
+            color: Colors.white,
+            child: Container(
+              height: 70,
+              padding: const EdgeInsets.only(bottom: 10, top: 5),
+              child: const TabBar(
+                //tab 하단 indicator size -> .label = label의 길이
+                //tab 하단 indicator size -> .tab = tab의 길이
+                indicatorSize: TabBarIndicatorSize.label,
+                //tab 하단 indicator color
+                indicatorColor: Colors.blueAccent,
+                //tab 하단 indicator weight
+                indicatorWeight: 2,
+                //label color
+                labelColor: Colors.blueAccent,
+                //unselected label color
+                unselectedLabelColor: Colors.black38,
+                labelStyle: TextStyle(
+                  fontSize: 13,
+                ),
+                tabs: [
+                  Tab(
+                    icon: Icon(
+                      Icons.edit,
+                    ),
+                    text: 'Edit',
+                  ),
+                  Tab(
+                    icon: Icon(
+                      Icons.calendar_month,
+                    ),
+                    text: 'Calendar',
                   ),
                 ],
+              ),
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return AlertDialog(
+                    title: const Text('나의 할일'),
+                    actions: [
+                      TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            title = value;
+                          });
+                        },
+                        decoration: const InputDecoration(hintText: "할일"),
+                      ),
+                      TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            description = value;
+                          });
+                        },
+                        decoration: const InputDecoration(hintText: "기간"),
+                      ),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                            setState(() {
+                              todos.add(Todo(
+                                title: title,
+                                description: description,
+                              ));
+                            });
+                          },
+                          child: const Text(" 추가하기"),
+                        ),
+                      ),
+                    ],
+                  );
+                },
               );
             },
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        unselectedItemColor: Colors.black, // Set the color to black
-        selectedItemColor: Colors.blueAccent,
-        currentIndex: _currentIndex,
-        onTap: _onBottomNavigationBarItemTapped,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.edit),
-            label: "",
+            child: const Icon(Icons.add),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: "",
-          ),
-        ],
+        ),
       ),
     );
   }
