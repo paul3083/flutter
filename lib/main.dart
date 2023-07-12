b210c05432ec7c9f3ba61bafbb7505cfce849b96
paul3083 <paul30833083@gmail.com>
2023-07-12 12:36:26 +0900

    todolist_bottomnavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:todolist/analyze.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,23 +35,22 @@ class TodoApp extends StatefulWidget {
 }
 
 class _TodoAppState extends State<TodoApp> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _widgetOptions = <Widget>[
+    const analyze()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   String title = "";
   String description = "";
 
   List<Todo> todos = [];
-
-  int _currentIndex = 0;
-
-  void _onBottomNavigationBarItemTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-      if (_currentIndex == 1) {
-        Navigator.pushNamed(
-            context, '/calendar'); // Navigate to the calendar screen
-        _currentIndex = 0;
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,85 +60,23 @@ class _TodoAppState extends State<TodoApp> {
         length: 2,
         child: Scaffold(
           appBar: AppBar(
-            title: const Text("Todo List"),
-          ),
-          body: TabBarView(
-            children: [
-              ListView.builder(
-                itemCount: todos.length,
-                itemBuilder: (_, index) {
-                  final todo = todos[index];
-                  return ListTile(
-                    title: Text(todo.title),
-                    subtitle: Text(todo.description),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.check),
-                      onPressed: () {
-                        setState(() {
-                          todos.removeAt(index);
-                        });
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("삭제되었습니다.")),
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-              TableCalendar(
-                firstDay: DateTime.utc(2021, 10, 16),
-                lastDay: DateTime.utc(2030, 3, 14),
-                focusedDay: DateTime.now(),
-                calendarBuilders: CalendarBuilders(
-                    markerBuilder: (context, date, dynamic event) {
-                  if (event.isNotEmpty) {
-                    return Container(
-                      width: 35,
-                      decoration: BoxDecoration(
-                          color: Colors.blueAccent.withOpacity(0.2),
-                          shape: BoxShape.circle),
-                    );
-                  }
-                  return null;
-                }),
-              )
-            ],
-          ),
-          extendBodyBehindAppBar: true,
-          bottomNavigationBar: Container(
-            color: Colors.white,
-            child: Container(
-              height: 70,
-              padding: const EdgeInsets.only(bottom: 10, top: 5),
-              child: const TabBar(
-                //tab 하단 indicator size -> .label = label의 길이
-                //tab 하단 indicator size -> .tab = tab의 길이
-                indicatorSize: TabBarIndicatorSize.label,
-                //tab 하단 indicator color
-                indicatorColor: Colors.blueAccent,
-                //tab 하단 indicator weight
-                indicatorWeight: 2,
-                //label color
-                labelColor: Colors.blueAccent,
-                //unselected label color
-                unselectedLabelColor: Colors.black38,
-                labelStyle: TextStyle(
-                  fontSize: 13,
+            backgroundColor: Colors.white,
+            title: const Padding(
+              padding: EdgeInsets.only(right: 240),
+              child: Text(
+                "투두프렌즈",
+                style: TextStyle(
+                  color: Colors.lightBlueAccent,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 22,
                 ),
-                tabs: [
-                  Tab(
-                    icon: Icon(
-                      Icons.edit,
-                    ),
-                    text: 'Edit',
-                  ),
-                  Tab(
-                    icon: Icon(
-                      Icons.calendar_month,
-                    ),
-                    text: 'Calendar',
-                  ),
-                ],
+              ),
+            ),
+            leading: const Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Icon(
+                Icons.messenger_outline,
+                color: Colors.lightBlueAccent,
               ),
             ),
           ),
@@ -187,6 +124,21 @@ class _TodoAppState extends State<TodoApp> {
               );
             },
             child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.edit),
+                label: 'Edit'
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bar_chart),
+                label: 'Chart'
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.lightBlueAccent,
+            onTap: _onItemTapped,
           ),
         ),
       ),
